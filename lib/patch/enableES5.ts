03c46db63e1bd26879579beb673b18b125f295aa
jian.gao <jian.gao@shopee.com>
2022-07-14 09:45:30 +0800

    feat: add ownkeys support

--- lib/patch/enableES5.ts
+++ lib/patch/enableES5.ts
@@ -2,16 +2,25 @@ import { enableES5 as immerEnableES5 } from "immer";
 import { replaceNewProxy } from "proxy-memoize";
 import TinyProxyPolyfill from "./TinyProxyPolyfill.ts";
 
-const hasProxies =
-  typeof Proxy !== "undefined" &&
-  typeof Proxy.revocable !== "undefined" &&
-  typeof Reflect !== "undefined";
+type AnyObject = { [key: string]: any };
+
+const ownKeys: (target: AnyObject) => PropertyKey[] =
+  typeof Reflect !== "undefined" && Reflect.ownKeys
+    ? Reflect.ownKeys
+    : typeof Object.getOwnPropertySymbols !== "undefined"
+    ? (obj) =>
+        Object.getOwnPropertyNames(obj).concat(
+          Object.getOwnPropertySymbols(obj) as any
+        )
+    : Object.getOwnPropertyNames;
 
 function proxyMemoizeEnableES5() {
-  if (!hasProxies)
+  if (typeof Proxy === "undefined" || typeof Proxy.revocable === "undefined")
     replaceNewProxy(
       (target, handler) => new (TinyProxyPolyfill as any)(target, handler)
     );
+  if (typeof Reflect === "undefined")
+    self.Reflect = { ownKeys } as unknown as any;
 }
 
 export default function enableES5() {
